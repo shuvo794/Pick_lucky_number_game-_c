682fa5a1dc555eb195d419c2f3fdb07972594124
shuvo794 <shuvohossain794@gmail.com>
2022-11-16 17:19:19 +0600

    send

--- guusses_game.c
+++ guusses_game.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 int main (void){
 
     int Pick_Number ,given_number;
     time_t t;
-    
+
     srand((unsigned)time(&t));
 
     Pick_Number = rand() %10 +1 ;
