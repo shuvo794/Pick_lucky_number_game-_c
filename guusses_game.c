15f76d4c58004c7fb8c8b64578a0df37e4639e7a
shuvo794 <shuvohossain794@gmail.com>
2022-11-16 17:08:27 +0600

    send

--- guusses_game.c
+++ guusses_game.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int main (void){
+    const int Pick_Number = 9;
+    int given_number;
+    printf("Please a chooche Number");
+scanf("%d",&given_number);
+
+if (Pick_Number == given_number)
+{
+    printf("You are win");
+}
+
+
 
     return 0;
 }
